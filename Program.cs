c084197f3d0bb9960ac0c00088fff7d45afb4b23
хэлп <ivanvanin41@gmail.com>
2023-01-24 10:31:40 +0300

    1 проект с api

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+namespace Nep_PR1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient();
+            var request = new RestRequest("https://api.coindesk.com/v1/bpi/currentprice.json", Method.Get);
+            var response = client.Execute(request);
+            var result = JsonConvert.DeserializeObject<ModelsApi.Rootobject>(response.Content);
+            Console.WriteLine(result.bpi.USD.rate);
+            Console.WriteLine(result.bpi.USD.description);
+           
+            Console.ReadLine();
+
+        }
+    }
+}
